56445927b50c1b52e814ea980402065224398d05
Hixon1000 <86545468+hixon1000@users.noreply.github.com>
2024-08-20 15:15:03 +0400

    Update template.lua

--- template.lua
+++ template.lua
@@ -1,18 +1,21 @@
 local dfpwm = require("cc.audio.dfpwm")
+
 local function play_sound(file_path)
-    -- local speaker = peripheral.find("speaker")
     local speakers = table.pack(peripheral.find("speaker"))
     local decoder = dfpwm.make_decoder()
+
     for chunk in io.lines(file_path, 16 * 1024) do
         local buffer = decoder(chunk)
         local funcs = {}
-        for i = 1, #speakers do
+
+        for i = 1, speakers.n do
             funcs[i] = function()
-            while not speaker[i].playAudio(buffer) do
-                os.pullEvent("speaker_audio_empty")
-            end
+                while not speakers[i].playAudio(buffer) do
+                    os.pullEvent("speaker_audio_empty")
+                end
             end
         end
+
         parallel.waitForAll(table.unpack(funcs, 1, speakers.n))
     end
 end
