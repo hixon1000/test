54798c4ed48fb47581444f3708546573f38c8337
Hixon1000 <86545468+hixon1000@users.noreply.github.com>
2024-08-20 19:18:24 +0400

    Update template.lua

--- template.lua
+++ template.lua
@@ -1,34 +1,35 @@
 local dfpwm = require("cc.audio.dfpwm")
 
+local function delay_playback(start_time)
+    while os.clock() < start_time do
+        os.pullEvent("timer")
+    end
+end
+
 local function play_sound(file_path)
-    -- Find all speakers
     local all_speakers = table.pack(peripheral.find("speaker"))
-    
-    -- Select only the first two unique speakers
     local speakers = table.pack(all_speakers[1], all_speakers[2])
     local decoder = dfpwm.make_decoder()
 
-    -- Verify the speakers being used
-    print("Number of speakers used: " .. speakers.n)
-    for i = 1, speakers.n do
-        print("Speaker " .. i .. ": " .. tostring(speakers[i]))
-    end
-
-    -- Play sound through the selected speakers
     for chunk in io.lines(file_path, 16 * 1024) do
         local buffer = decoder(chunk)
-        local funcs = {}
+        local start_time = os.clock() + 0.1 -- 100ms delay
 
-        for i = 1, speakers.n do
-            funcs[i] = function()
-                while not speakers[i].playAudio(buffer) do
-                    os.pullEvent("speaker_audio_empty")
+        -- Function to play audio on all speakers
+        local function play_audio_on_all_speakers()
+            local funcs = {}
+            for i = 1, speakers.n do
+                funcs[i] = function()
+                    delay_playback(start_time)
+                    while not speakers[i].playAudio(buffer) do
+                        os.pullEvent("speaker_audio_empty")
+                    end
                 end
             end
+            parallel.waitForAll(table.unpack(funcs, 1, speakers.n))
         end
 
-        -- Ensure synchronization across the selected speakers
-        parallel.waitForAll(table.unpack(funcs, 1, speakers.n))
+        play_audio_on_all_speakers()
     end
 end
 
