5c5917094edd6387f754c07053e0fa9e04588150
Hixon1000 <86545468+hixon1000@users.noreply.github.com>
2024-08-20 17:37:29 +0400

    Update template.lua

--- template.lua
+++ template.lua
@@ -4,18 +4,24 @@ local function play_sound(file_path)
     local speakers = table.pack(peripheral.find("speaker"))
     local decoder = dfpwm.make_decoder()
 
-    for chunk in io.lines(file_path, 16 * 1024) do
-        local buffer = decoder(chunk)
-        local funcs = {}
-
-        for i = 1, speakers.n do
-            funcs[i] = function()
-                while not speakers[i].playAudio(buffer) do
+    -- Function to play a chunk on all speakers
+    local function play_chunk_on_all_speakers(buffer)
+        local all_speakers_ready = false
+        while not all_speakers_ready do
+            all_speakers_ready = true
+            for i = 1, speakers.n do
+                if not speakers[i].playAudio(buffer) then
+                    all_speakers_ready = false
                     os.pullEvent("speaker_audio_empty")
+                    break
                 end
             end
         end
+    end
 
-        parallel.waitForAll(table.unpack(funcs, 1, speakers.n))
+    -- Iterate through the file and play each chunk
+    for chunk in io.lines(file_path, 16 * 1024) do
+        local buffer = decoder(chunk)
+        play_chunk_on_all_speakers(buffer)
     end
 end
